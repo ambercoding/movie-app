20c6830fcc94d9caac4a760b6e673efd751da262
Amber Coding <a.mariecarter@yahoo.com>
2016-12-21 10:13:15 -0500

    movie app setup

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function(){
+	console.log('test');
+
+	var apiBaseUrl = 'http://api.themoviedb.org/3/';
+	var imageBaseUrl = 'http://image.tmdb.org/t/p/';
+	const nowPlayingUrl = apiBaseUrl + 'movies/now_playing'
+});
